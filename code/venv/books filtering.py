925f955744f095bf6d36bbb5b637245f70ce1f35
mattl1598 <37371392+mattl1598@users.noreply.github.com>
2018-05-24 17:43:54 +0100

    google books code
    google books code migrated from repl.it into pycharm

---
+++ code/venv/books filtering.py
@@ -0,0 +1,57 @@
+import googlebooks
+import misc_python
+
+
+def getBook():
+	api = googlebooks.Api()
+	isbn = "isbn:"
+	isbn += input("input isbn")
+	deets = api.list(isbn)
+	return str(deets)
+
+
+def getTitle(data):
+	datastart = data.find('title') + 11
+	dataend = data.find('authors') - 4
+	return (data[datastart:dataend])
+
+
+def getAuthor(data):
+	datastart = data.find('authors') + 12
+	dataend = data.find(']') - 1
+	return (data[datastart:dataend])
+
+
+def getGenre(data):
+	datastart = data.find('categories') + 15
+	dataend = data.find("averageRating") - 5
+	return (data[datastart:dataend])
+
+
+def getReleased(data):
+	datastart = data.find('publishedDate') + 17
+	dataend = data.find("description") - 4
+	return (data[datastart:dataend])
+
+
+#BINDING???
+
+
+def getAge(data):
+	datastart = data.find('maturityRating') + 18
+	dataend = data.find("allowAnonLogging") - 4
+	return (data[datastart:dataend])
+
+
+def getBlurb(data):
+
+	datastart = data.find('description') + 15
+	dataend = data.find("industryIdentifiers") - 4
+	return (data[datastart:dataend])
+
+
+def getImageURL(data):
+	pass
+
+
+print(getBlurb(getBook()))
